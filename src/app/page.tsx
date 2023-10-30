1e91cd49f256c5b935d10fcacf771ae60a6e6472
KuharchukDmytro <kuharchuk.dmytro1337@gmail.com>
2023-10-30 14:52:20 +0200

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,6 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-
-
-
-
-
-
-
-        
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
